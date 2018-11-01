8780ab70ecd1d05bd9c6ea9a6f5dfa5c8a70f656
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-01 11:45:35 -0500

    NewPost add state

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import classes from './NewPost.scss';
 
 class newPost extends Component {
-  state = {};
+  state = {
+    title: '',
+    content: '',
+    autor: 'Virginia',
+  };
 
   render() {
     return (
@@ -11,25 +15,38 @@ class newPost extends Component {
           <h4>Create Post</h4>
           <form>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Title" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Title"
+                value={this.state.title}
+                onChange={ev => this.setState({ title: ev.target.value })}
+              />
             </div>
             <div className="form-group">
               <textarea
                 className="form-control"
                 placeholder="Contenidor"
                 rows="4"
+                value={this.state.content}
+                onChange={ev => this.setState({ content: ev.target.value })}
               />
             </div>
             <div className="form-group">
               <i className="fas fa-angle-down" />
-              <select className="form-control">
-                <option>Virginia</option>
-                <option>Carolina</option>
-                <option>Diego</option>
-                <option>Ximena</option>
+              <select
+                className="form-control"
+                value={this.state.autor}
+                onChange={ev => this.setState({ autor: ev.target.value })}
+              >
+                <option value="Virginia">Virginia</option>
+                <option value="Carolina">Carolina</option>
+                <option value="Diego">Diego</option>
+                <option value="Ximena">Ximena</option>
               </select>
             </div>
           </form>
+          <button className={classes.btnAdd}>Add Post</button>
         </div>
       </>
     );
