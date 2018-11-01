39a16f78f83706db998fc1ed00a515dfdc35825d
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-01 11:25:56 -0500

    Decoracion NewPost y add fontawesome

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classes from './NewPost.scss';
 
-const NewPost = () => (
-  <>
-    <h3>NewPost</h3>
-  </>
-);
+class newPost extends Component {
+  state = {};
 
-export default NewPost;
+  render() {
+    return (
+      <>
+        <div className={classes.NewPost}>
+          <h4>Create Post</h4>
+          <form>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Title" />
+            </div>
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                placeholder="Contenidor"
+                rows="4"
+              />
+            </div>
+            <div className="form-group">
+              <i className="fas fa-angle-down" />
+              <select className="form-control">
+                <option>Virginia</option>
+                <option>Carolina</option>
+                <option>Diego</option>
+                <option>Ximena</option>
+              </select>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+export default newPost;
