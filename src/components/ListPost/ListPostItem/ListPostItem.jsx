2b675799d09b5f28b711c20bcd6c09ebe6e8def7
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-01 17:39:24 -0500

    ListPostItem decoracion title

--- src/components/ListPost/ListPostItem/ListPostItem.jsx
+++ src/components/ListPost/ListPostItem/ListPostItem.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import classes from './ListPostItem.scss';
 
-const ListPostItem = ({ title, autor }) => (
-  <>
-    <div className="col-4">
-      <div className={classes.ListPostItem}>
-        <h5>{title}</h5>
-        <p className={classes.Autor}>{autor}</p>
+const ListPostItem = ({ title, autor }) => {
+  let shorText;
+  const size = title.length;
+
+  if (size <= 20) {
+    shorText = title.slice(0, 20);
+  } else {
+    shorText = title.slice(0, 50) + '...';
+  }
+
+  return (
+    <>
+      <div className="col-4">
+        <div className={classes.ListPostItem}>
+          <h5>{shorText}</h5>
+          <p className={classes.Autor}>{autor}</p>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default ListPostItem;
