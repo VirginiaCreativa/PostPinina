2f3a209577698810715ed0ce0da7000ead18af2a
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-01 18:51:31 -0500

    title FullPost

--- src/components/FullPost/FullPost.jsx
+++ src/components/FullPost/FullPost.jsx
@@ -5,14 +5,17 @@ const FullPost = ({ selected, title, content, autor, onDeleted }) => {
   let postDetalle;
   if (selected) {
     postDetalle = (
-      <div className={classes.FullPost}>
-        <button onClick={onDeleted} className={classes.btnDelete}>
-          X
-        </button>
-        <h3>{title}</h3>
-        <p>{content}</p>
-        <p className={classes.Autor}>{autor}</p>
-      </div>
+      <>
+        <h6>Full Post</h6>
+        <div className={classes.FullPost}>
+          <button onClick={onDeleted} className={classes.btnDelete}>
+            X
+          </button>
+          <h3>{title}</h3>
+          <p>{content}</p>
+          <p className={classes.Autor}>{autor}</p>
+        </div>
+      </>
     );
   }
   return <>{postDetalle}</>;
