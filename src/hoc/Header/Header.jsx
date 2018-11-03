a3c95acd8a4b0406c7a7c6047e122c262b5b34fe
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-11-03 14:53:10 -0500

    Create navigation

--- src/hoc/Header/Header.jsx
+++ src/hoc/Header/Header.jsx
@@ -4,11 +4,38 @@ import classes from './Header.scss';
 const header = () => (
   <>
     <header className={classes.Header}>
-      <div className={classes.Box}>
-        <h1>
-          Post
-          <span>Pinina</span>
-        </h1>
+      <div className="container-full">
+        <div className="row">
+          <div className="col">
+            <div className={classes.Logo}>
+              <h1>
+                Post
+                <span>Pinina</span>
+              </h1>
+            </div>
+          </div>
+          <div className="col">
+            <nav className={classes.Nav}>
+              <ul className="nav justify-content-end">
+                <li className="nav-item">
+                  <a className="nav-link active" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/newpost">
+                    New Post
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </header>
   </>
